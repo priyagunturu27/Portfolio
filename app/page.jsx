824de8f2e0f6204e8b657ccb8562382ce173f88f
priyagunturu27 <gpriya.dev27@gmail.com>
2025-03-15 21:48:56 -0500

    chore:Added profile

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 				home page
 				<div className='flex flex-col xl:flex-row justify-between items-center xl:pt-8 xl:pb-24'>
 					{/* text */}
-					<div className='text-center xl:text-left'>
+					<div className='text-center xl:text-left order-2 xl:order-none'>
 						<span className='text-xl'>Software Developer</span>
 						<h1 className='h1'>
 							Hello I'm <br />{' '}
@@ -39,7 +39,7 @@ const Home = () => {
 						</div>
 					</div>
 
-					<div>
+					<div className='order-1 xl:order-none'>
 						<Photo />
 					</div>
 				</div>
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+'use client'
+import { Opacity } from '@material-ui/icons'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
 
 const Photo = () => {
-	return <div>Photo</div>
+	return (
+		<div className='w-full h-full relative'>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{
+					opacity: 1,
+					transition: { delay: 2, duration: 0.4, ease: 'easeIn' },
+				}}>
+				<div className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]'>
+					<Image
+						src='/assets/photo.jpeg'
+						priority
+						fill
+						quality={100}
+						className='object-contain'
+						alt=''
+					/>
+				</div>
+			</motion.div>
+		</div>
+	)
 }
 
 export default Photo
